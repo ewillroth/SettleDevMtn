5b2c27b4d4ba95af7e71882ccdda4e89aaef42d3
Eric Willroth <ewillroth@gmail.com>
2019-02-02 14:17:57 -0600

    update suggestions table when user logs in

--- db/users/update_settles.sql
+++ db/users/update_settles.sql
@@ -5,3 +5,7 @@ WHERE user_id = $2;
 UPDATE settles
 SET creator_id = (SELECT user_id FROM users WHERE email = $1)
 WHERE creator_id = $2;
+
+UPDATE suggestions
+SET user_id = (SELECT user_id FROM users WHERE email = $1)
+WHERE user_id = $2;
